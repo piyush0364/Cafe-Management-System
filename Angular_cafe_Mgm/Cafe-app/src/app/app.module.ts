133f9566df05db2171fd1908c01a58b09fca8834
Apoorva <apoorva.urvi1112@gmail.com>
2024-10-14 23:50:09 +0530

    feedback admin

--- Angular_cafe_Mgm/Cafe-app/src/app/app.module.ts
+++ Angular_cafe_Mgm/Cafe-app/src/app/app.module.ts
@@ -1,3 +1,66 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { MenuComponent } from './menu/menu.component';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { DashboardComponent } from './admin/dashboard/dashboard.component';
+// import { CategoriesComponent } from './admin/categories/categories.component';
+// import { ProductsComponent } from './admin/products/products.component';
+// import { OrdersComponent } from './admin/orders/orders.component';
+// import { Contact } from './Models/contact.model';
+// import { ContactComponent } from './admin/contact/contact.component';
+
+// import { LoginComponent } from './login/login.component';
+// import { SignupComponent } from './signup/signup.component';
+// import { CustomersComponent } from './admin/customers/customers.component';
+// import { HomeComponent } from './home/home.component';
+// import { NavComponent } from './nav/nav.component';
+// import { FeedbackComponent } from './feedback/feedback.component';
+// import { CartComponent } from './cart/cart.component';
+// import { HomoComponent } from './homo/homo.component';
+// import { AboutComponent } from './about/about.component';
+// import { PaymentComponent } from './payment/payment.component';
+// import { RouterModule, ROUTES } from '@angular/router';
+// import { FeedbackAdminComponent } from './admin/feedback/feedback.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     MenuComponent,
+//     DashboardComponent,
+//     CategoriesComponent,
+//     ProductsComponent,
+//     OrdersComponent,
+//     CustomersComponent,
+//     ContactComponent,
+//     LoginComponent,
+//     SignupComponent,
+//     HomeComponent,
+//     NavComponent,
+//     FeedbackComponent,
+//     CartComponent,
+//     HomoComponent,
+//     AboutComponent,
+//     PaymentComponent,
+//     FeedbackAdminComponent
+//   ],
+  
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     HttpClientModule,
+//     ReactiveFormsModule,
+//     RouterModule
+  
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent,DashboardComponent,CategoriesComponent,ProductsComponent,OrdersComponent,CustomersComponent,ContactComponent]
+// })
+// export class AppModule { }
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -23,8 +86,7 @@ import { CartComponent } from './cart/cart.component';
 import { HomoComponent } from './homo/homo.component';
 import { AboutComponent } from './about/about.component';
 import { PaymentComponent } from './payment/payment.component';
-import { RouterModule, ROUTES } from '@angular/router';
-import { FeedbackAdminComponent } from './admin/feedback/feedback.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +107,7 @@ import { FeedbackAdminComponent } from './admin/feedback/feedback.component';
     HomoComponent,
     AboutComponent,
     PaymentComponent,
-    FeedbackAdminComponent
+    FooterComponent
   ],
   
   imports: [
@@ -53,9 +115,7 @@ import { FeedbackAdminComponent } from './admin/feedback/feedback.component';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    RouterModule
-  
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent,DashboardComponent,CategoriesComponent,ProductsComponent,OrdersComponent,CustomersComponent,ContactComponent]
